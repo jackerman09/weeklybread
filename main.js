a766ecbecd37c0ac3004513c8d15d616ac9de824
Jackerman09 <jackerman09@gmail.com>
2019-06-04 07:50:07 -0400

    add console log to buttons

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
 	console.log("test")
 	$('#button-level-1').click(function() {
 		ga('send', 'event', 'button', 'click', 'button-level-1');
+		console.log('click 1')
 	});
 	$('#button-level-2').click(function() {
 		ga('send', 'event', 'button', 'click', 'button-level-2');
+		console.log('click 2')
 	});
 	$('#button-level-3').click(function() {
 		ga('send', 'event', 'button', 'click', 'button-level-3');
+		console.log('click 3')
 	});
 });
