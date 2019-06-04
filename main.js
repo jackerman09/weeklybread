0bd908e72a793bed85f9b0dd0dca1899aa55bcb8
Jackerman09 <jackerman09@gmail.com>
2019-06-04 13:30:46 -0400

    switch ga to gtag

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
-	console.log("test")
+	console.log("page loaded")
 	$('#button-level-1').click(function() {
-		ga('send', 'event', 'button', 'click', 'button-level-1');
+		gtag('event', 'click', {
+		  'event_category': 'button',
+		  'event_label': 'button-level-1'
+		});
 		console.log('click 1')
 	});
 	$('#button-level-2').click(function() {
-		ga('send', 'event', 'button', 'click', 'button-level-2');
+		gtag('event', 'click', {
+		  'event_category': 'button',
+		  'event_label': 'button-level-2'
+		});
 		console.log('click 2')
 	});
 	$('#button-level-3').click(function() {
-		ga('send', 'event', 'button', 'click', 'button-level-3');
+		gtag('event', 'click', {
+		  'event_category': 'button',
+		  'event_label': 'button-level-3'
+		});
 		console.log('click 3')
 	});
 	$('#mc-embedded-subscribe').click(function() {
-		ga('send', 'event', 'button', 'click', 'subscribed');
+		gtag('event', 'click', {
+		  'event_category': 'button',
+		  'event_label': 'subscribed'
+		});
 		console.log('subscribed')
 	});
 });
