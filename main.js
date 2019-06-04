6871d68f3422469240f92c4c8750a8f177769c06
Jackerman09 <jackerman09@gmail.com>
2019-06-04 13:41:10 -0400

    remove console logs

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-	console.log("page loaded")
-
 	$('#button-level-1').click(function() {
 		gtag('event', 'click', {
 		  'event_category': 'button',
 		  'event_label': 'button-level-1'
 		});
-		console.log('click 1');
 	});
 
 	$('#button-level-2').click(function() {
@@ -14,7 +11,6 @@ $(document).ready(function(){
 		  'event_category': 'button',
 		  'event_label': 'button-level-2'
 		});
-		console.log('click 2');
 	});
 
 	$('#button-level-3').click(function() {
@@ -22,7 +18,6 @@ $(document).ready(function(){
 		  'event_category': 'button',
 		  'event_label': 'button-level-3'
 		});
-		console.log('click 3');
 	});
 
 	$('#mc-embedded-subscribe').click(function() {
@@ -30,7 +25,6 @@ $(document).ready(function(){
 		  'event_category': 'button',
 		  'event_label': 'subscribed'
 		});
-		console.log('subscribed');
 	});
-	
+
 });
