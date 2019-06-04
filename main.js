f2952fd11a2db98ab602e50c494fa454d8c60cb0
Jackerman09 <jackerman09@gmail.com>
2019-06-04 13:39:02 -0400

    js formatting

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
 	console.log("page loaded")
+
 	$('#button-level-1').click(function() {
 		gtag('event', 'click', {
 		  'event_category': 'button',
 		  'event_label': 'button-level-1'
 		});
-		console.log('click 1')
+		console.log('click 1');
 	});
+
 	$('#button-level-2').click(function() {
 		gtag('event', 'click', {
 		  'event_category': 'button',
 		  'event_label': 'button-level-2'
 		});
-		console.log('click 2')
+		console.log('click 2');
 	});
+
 	$('#button-level-3').click(function() {
 		gtag('event', 'click', {
 		  'event_category': 'button',
 		  'event_label': 'button-level-3'
 		});
-		console.log('click 3')
+		console.log('click 3');
 	});
+
 	$('#mc-embedded-subscribe').click(function() {
 		gtag('event', 'click', {
 		  'event_category': 'button',
 		  'event_label': 'subscribed'
 		});
-		console.log('subscribed')
+		console.log('subscribed');
 	});
+	
 });
